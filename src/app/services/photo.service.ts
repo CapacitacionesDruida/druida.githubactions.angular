b66bfd6bd61ca83a5e9bd71d21584ae5cbb92481
Juan David Yepes González <judayego@outlook.com>
2023-11-09 10:58:51 -0500

    Versión del yaml para desplegar con contenedores

--- src/app/services/photo.service.ts
+++ src/app/services/photo.service.ts
@@ -2,18 +2,19 @@ import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
 import { HttpClient } from '@angular/common/http';
 import { Photo } from '../models/photo.model';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PhotoService {
 
-  readonly URL_API = environment.apiUrl;
+  readonly URL_API = environment.apiUrl as unknown as string;
 
   constructor(private http: HttpClient) { }
   
   public getPhotos(): Observable<Photo[]> {
+    console.log(this.URL_API);    
     return this.http.get<Photo[]>(this.URL_API);
   }
 }
--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: false,
-    apiUrl: 'https://jsonplaceholder.typicode.com/photos'
+    apiUrl: window[<any>'env'][<any>'apiUrl']
 };
